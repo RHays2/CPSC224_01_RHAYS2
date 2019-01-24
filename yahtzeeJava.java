8c3b29eb1753e255eb76d2af8668ba60ea5ce617
unknown <ryanhays02@yahoo.com>
2019-01-24 10:23:16 -0800

    Converting yahtzee.cpp to java

--- yahtzeeJava.java
+++ yahtzeeJava.java
@@ -1,1 +1,35 @@
-file
+
+public class yahtzee1
+{
+
+	public static void main(String[] args)
+  {
+		const int DICE_IN_PLAY = 5;
+    int hand[DICE_IN_PLAY];
+    //srand(time(0))
+    char playAgain = 'y';
+
+	}
+  public static int rollDie()
+  {
+
+  }
+  public static int maxOfAKindFound(int [])
+  {
+
+  }
+  public static int maxStraightFound(int [])
+  {
+
+  }
+  public static bool fullHouseFound(int [])
+  {
+
+  }
+  public static int totalAllDice(int [])
+  {
+
+  }
+  public static void sortArray(int [], int)
+
+}
